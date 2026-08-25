8acd3513d791b479afca9237185bfe7817d5544a
Alif Januar Rizky <caturharjodigital@gmail.com>
2026-08-25 08:44:05 +0700

    fix: move compilerOptions to svelte.config to unblock vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	preprocess: vitePreprocess(),
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => (filename.includes('node_modules') ? undefined : true)
+	},
 	kit: {
 		adapter: adapter({ runtime: 'nodejs20.x' })
 	}
